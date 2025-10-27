54c61105cd501add9facc181709482fe75a687a0
whiffy <bryanzhao168@yahoo.ca>
2025-10-26 22:18:50 -0400

    Added URL to fetch server url

---
+++ front_end/connect-front-end/utilities/fetchApiUrl.ts
@@ -0,0 +1,7 @@
+export default function getServerUrl() {
+  if (process.env.NEXT_PUBLIC_DEV == "true" || !process.env.NEXT_PUBLIC_DEV) {
+    return "http://localhost:3003";
+  }
+
+  return process.env.NEXT_PUBLIC_SERVER_URL_PROD;
+}
