edf90ea1e5844c1193e1dd30d5c407eae9eee902
whiffy <bryanzhao168@yahoo.ca>
2025-11-05 20:16:52 -0500

    Moved get handlers to the proper get folder

--- back_end/connect-back-end/server/startServer.ts
+++ back_end/connect-back-end/server/startServer.ts
@@ -3,11 +3,13 @@ import { PrismaClient } from "@prisma/client";
 import pino from "pino";
 import express from "express";
 import cors from "cors";
-import getStudentGradesForTest from "../handlers/get-student-grades-for-test/getStudentGradesForTest";
-import getAllStudentGradesForClass from "../handlers/get-all-student-grades-for-class/getAllStudentGradesForClass";
-import getClassInformation from "../handlers/get-class-information/getClassInformation";
-import getClasses from "../handlers/get-classes/getClasses";
+import getStudentGradesForTest from "../handlers/get/student-grades-for-test/getStudentGradesForTest";
+import getAllStudentGradesForClass from "../handlers/get/all-student-grades-for-class/getAllStudentGradesForClass";
+import getClassInformation from "../handlers/get/class-information/getClassInformation";
+import getClasses from "../handlers/get/classes/getClasses";
 import createTestForClass from "../handlers/post/create-test-for-class/createTestForClass";
+import studentTestUploadPresignedUrl from "../handlers/get/student-test-upload-presigned-url/studentTestUploadPresignedUrl";
+import publishStudentSubmissionEvent from "../handlers/post/mark-test-for-class/publish/publishStudentSubmissionEvent";
 
 export default async function startServer(databaseClient: PrismaClient) {
   const logger = pino({
@@ -31,11 +33,19 @@ export default async function startServer(databaseClient: PrismaClient) {
     "/classes/:classId/grades/tests/:testId",
     getStudentGradesForTest(databaseClient)
   );
+  app.get(
+    "/classes/:classId/students/:studentId/tests/:testId/grade",
+    studentTestUploadPresignedUrl()
+  );
 
   // post handlers
   app.post(
     "/classes/:classId/tests/create",
     createTestForClass(databaseClient)
+  );
+  app.post(
+    "/classes/:classId/students/:studentId/tests/:testId/grade",
+    publishStudentSubmissionEvent()
   );
 
   app.listen(SERVER_PORT, () => {
