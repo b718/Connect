11a084d5e43d5916052b7dfe8b7b9c70b2df09b1
whiffy <bryanzhao168@yahoo.ca>
2025-11-06 22:32:28 -0500

    Added handler to submit student submission

---
+++ back_end/connect-back-end/handlers/post/mark-test-for-class/publish/publishStudentSubmissionEvent.ts
@@ -0,0 +1,73 @@
+import { SQSClient } from "@aws-sdk/client-sqs";
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import pino from "pino";
+import { publishStudentSubmissionToQueue } from "./publishStudentSubmissionToQueue";
+
+type PublishStudentSubmissionEventResponse = {
+  statusCode: number;
+  message: string;
+};
+
+function createPublishStudentSubmissionEventResponse(
+  statusCode: number,
+  message: string,
+  res: Response
+) {
+  const response: PublishStudentSubmissionEventResponse = {
+    statusCode: statusCode,
+    message: message,
+  };
+
+  res.status(statusCode).json(response);
+}
+
+export default function publishStudentSubmissionEvent() {
+  const successMessage = "successfully published student submission event";
+  const errorMessage = "unsuccessfully published student submission event";
+  const logger = pino({
+    name: "handlers/post/mark-test-for-class/publish/publishStudentSubmissionEvent.ts",
+  });
+  const studentSubmissionQueueClient = new SQSClient({});
+
+  return async function (req: Request, res: Response) {
+    const classId = req.params.classId;
+    const studentId = req.params.studentId;
+    const testId = req.params.testId;
+
+    try {
+      const studentSubmission = await publishStudentSubmissionToQueue(
+        classId,
+        studentId,
+        testId,
+        studentSubmissionQueueClient
+      );
+
+      logger.info({
+        classId: classId,
+        testId: testId,
+        message: successMessage,
+      });
+
+      createPublishStudentSubmissionEventResponse(
+        StatusCodes.OK,
+        successMessage,
+        res
+      );
+    } catch (error) {
+      console.log(error);
+      logger.info({
+        classId: classId,
+        testId: testId,
+        error: error,
+        message: errorMessage,
+      });
+
+      createPublishStudentSubmissionEventResponse(
+        StatusCodes.INTERNAL_SERVER_ERROR,
+        errorMessage,
+        res
+      );
+    }
+  };
+}
---
+++ back_end/connect-back-end/handlers/post/mark-test-for-class/publish/publishStudentSubmissionToQueue.ts
@@ -0,0 +1,49 @@
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+
+function createMessageGroupId(classId: string, testId: string) {
+  return classId + ":" + testId;
+}
+
+function createTestSubmissionMessage(
+  classId: string,
+  studentId: string,
+  testId: string
+) {
+  return `Test '${testId}' submission for student '${studentId}' on ${new Date()} for class '${classId}'`;
+}
+
+export async function publishStudentSubmissionToQueue(
+  classId: string,
+  studentId: string,
+  testId: string,
+  studentSubmissionQueueClient: SQSClient
+) {
+  const studentSubmissionQueueUrl = process.env.STUDENT_SUBMISSION_QUEUE_URL;
+  const studentSubmission = new SendMessageCommand({
+    QueueUrl: studentSubmissionQueueUrl,
+    DelaySeconds: 10,
+    MessageAttributes: {
+      ClassId: {
+        DataType: "String",
+        StringValue: classId,
+      },
+      StudentId: {
+        DataType: "String",
+        StringValue: studentId,
+      },
+      TestId: {
+        DataType: "String",
+        StringValue: testId,
+      },
+    },
+    MessageBody: createTestSubmissionMessage(classId, studentId, testId),
+  });
+
+  try {
+    await studentSubmissionQueueClient.send(studentSubmission);
+  } catch (error) {
+    throw new Error(
+      `faced error: ${error} when trying to submit student submission`
+    );
+  }
+}
