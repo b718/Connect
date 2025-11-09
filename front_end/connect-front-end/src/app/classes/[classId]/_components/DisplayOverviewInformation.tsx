918f74f227617ee1c49d80c59b44cb21cb84814b
whiffy <bryanzhao168@yahoo.ca>
2025-11-08 20:58:09 -0500

    Added new test page view

--- front_end/connect-front-end/src/app/classes/[classId]/_components/DisplayOverviewInformation.tsx
+++ front_end/connect-front-end/src/app/classes/[classId]/_components/DisplayOverviewInformation.tsx
@@ -2,14 +2,25 @@ import React, { FC } from "react";
 import { CategorizedTests } from "../_utilities/fetch-student-grades-for-class/categorizeTestsForStudents";
 import styles from "../page.module.css";
 
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from "react-tooltip";
+import { HelpCircle } from "@untitledui/icons";
+import { useRouter } from "next/navigation";
+
 interface DisplayOverviewInformationProps {
-  studentGrades: CategorizedTests;
   classId: string;
+  studentGrades: CategorizedTests;
 }
 
 const DisplayOverviewInformation: FC<DisplayOverviewInformationProps> = ({
+  classId,
   studentGrades,
 }) => {
+  const router = useRouter();
+  const redirectToStudentTest = (studentId: string, testId: string) => {
+    router.push(`/classes/${classId}/students/${studentId}/tests/${testId}`);
+  };
+
   return (
     <table className={styles.DisplayInformationTable}>
       <thead>
@@ -37,18 +48,35 @@ const DisplayOverviewInformation: FC<DisplayOverviewInformationProps> = ({
             <td>{student.studentId}</td>
             <td>{student.firstName}</td>
             <td>{student.lastName}</td>
-            {student.testGrades.map((testGrade) => (
+            {student.testGrades.map((testGrade, index) => (
               <td>
-                {testGrade.viewStudentSubmissionUrl ? (
+                <div className={styles.StudentGradeCell}>
                   <a
                     target={"_blank"}
-                    href={testGrade.viewStudentSubmissionUrl}
+                    onClick={() =>
+                      redirectToStudentTest(
+                        student.studentId,
+                        studentGrades.tests[index].testId
+                      )
+                    }
                   >
                     {testGrade.grade.toFixed(2)}%
                   </a>
-                ) : (
-                  `${testGrade.grade.toFixed(2)}%`
-                )}
+                  {testGrade.manualInterventionRequired && (
+                    <div className={styles.StudentGradeCellToolTipContainer}>
+                      <a
+                        data-tooltip-id={"manual-intervention-required"}
+                        data-tooltip-content={
+                          "This test requires manual intervention."
+                        }
+                        data-tooltip-place={"top"}
+                      >
+                        <HelpCircle size={15} />
+                      </a>
+                      <Tooltip id={"manual-intervention-required"} />
+                    </div>
+                  )}
+                </div>
               </td>
             ))}
           </tr>
