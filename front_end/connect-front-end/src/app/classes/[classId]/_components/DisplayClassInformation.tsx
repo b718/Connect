43e5eec7aabd5623ea97724108ce57d60cd3814e
whiffy <bryanzhao168@yahoo.ca>
2025-11-11 20:23:59 -0500

    Putting into git for now

--- front_end/connect-front-end/src/app/classes/[classId]/_components/DisplayClassInformation.tsx
+++ front_end/connect-front-end/src/app/classes/[classId]/_components/DisplayClassInformation.tsx
@@ -1,52 +1,55 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import {
-  ClassInformation,
-  defaultClassInformation,
-  fetchClass,
-} from "../_utilities/fetchClass";
 import { useParams } from "next/navigation";
 import styles from "../page.module.css";
-import DisplayStudentsInformation from "./DisplayStudentsInformation";
-import DisplayTeachersInformation from "./DisplayTeachersInformation";
+import DisplayStudentsInformation from "./students/DisplayStudentsInformation";
+import DisplayTeachersInformation from "./teachers/DisplayTeachersInformation";
 import DisplayClassTab from "./DisplayClassTab";
+import { fetchStudentGrades } from "../_utilities/fetch-student-grades-for-class/fetchStudentGradesForClass";
+import { CategorizedTests } from "../_utilities/fetch-student-grades-for-class/categorizeTestsForStudents";
+import {
+  ClassInformation,
+  fetchClass,
+} from "../_utilities/fetch-class/fetchClass";
+import DisplayCreateTestInformation from "./create-test/DisplayCreateTestInformation";
+import DisplayClassOverviewInformation from "./class-overview-information/DisplayClassOverviewInformation";
 
 const TEACHERS = "Teachers";
 const STUDENTS = "Students";
 const OVERVIEW = "Overview";
+const CREATE_TEST = "Create Test";
 
-export type Tab = "Students" | "Teachers" | "Overview";
+export type Tab = "Students" | "Teachers" | "Overview" | "Create Test";
 
 const DisplayClassInformation = () => {
   const params = useParams();
-  const [classInformation, setClassInformation] = useState<ClassInformation>(
-    defaultClassInformation
-  );
+  const classId = params.classId as string;
+  const [classInformation, setClassInformation] = useState<ClassInformation>();
+  const [studentGrades, setStudentGrades] = useState<CategorizedTests>();
+  const [activeTab, setActiveTab] = useState<Tab>("Overview");
   const [error, setError] = useState<Error>();
-  const [activeTab, setActiveTab] = useState<Tab>("Students");
 
   useEffect(() => {
-    fetchClass(params.classId as string)
-      .then((classInformation) => {
-        setClassInformation(classInformation);
+    Promise.all([fetchClass(classId), fetchStudentGrades(classId)])
+      .then((results) => {
+        setClassInformation(results[0]);
+        setStudentGrades(results[1]);
       })
-      .catch((error) => {
-        setError(error);
-      });
-  }, [params.classId]);
+      .catch((error) => setError(error));
+  }, [classId]);
 
   if (error) {
     return (
-      <div className={styles.DisplayClassInformationContainer}>
+      <div className={styles.DisplayClassLoadingErrorContainer}>
         {"Error loading class, please try again"}
       </div>
     );
   }
 
-  if (classInformation === defaultClassInformation) {
+  if (!classInformation || !studentGrades) {
     return (
-      <div className={styles.DisplayClassInformationContainer}>
+      <div className={styles.DisplayClassLoadingErrorContainer}>
         {"Loading..."}
       </div>
     );
@@ -54,8 +57,9 @@ const DisplayClassInformation = () => {
 
   return (
     <div className={styles.DisplayClassInformationContainer}>
-      <h1>{classInformation.courseName}</h1>
-      <p>Grade: {classInformation.studentGradeYear}</p>
+      <h1 className={styles.DisplayClassInformationHeader}>
+        {`${classInformation.courseName}`}
+      </h1>
 
       <div className={styles.TabContainer}>
         <DisplayClassTab
@@ -78,14 +82,29 @@ const DisplayClassInformation = () => {
           count={classInformation.teachers.length}
           setTabValue={setActiveTab}
         />
+        <DisplayClassTab
+          activeTabValue={activeTab}
+          tabValue={CREATE_TEST}
+          tabDisplayValue={CREATE_TEST}
+          setTabValue={setActiveTab}
+        />
       </div>
 
       <div className={styles.TabContent}>
+        {activeTab === OVERVIEW && (
+          <DisplayClassOverviewInformation
+            studentGrades={studentGrades}
+            classId={classId}
+          />
+        )}
         {activeTab === STUDENTS && (
           <DisplayStudentsInformation students={classInformation.students} />
         )}
         {activeTab === TEACHERS && (
           <DisplayTeachersInformation teachers={classInformation.teachers} />
+        )}
+        {activeTab === CREATE_TEST && (
+          <DisplayCreateTestInformation classId={classId} />
         )}
       </div>
     </div>
