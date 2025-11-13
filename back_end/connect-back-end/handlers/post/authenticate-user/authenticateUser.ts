b3f58f756046a4ff338b9f0dc67b99a7fc8dda71
whiffy <bryanzhao168@yahoo.ca>
2025-11-12 19:40:50 -0500

    Added more to auth handler

--- back_end/connect-back-end/handlers/post/authenticate-user/authenticateUser.ts
+++ back_end/connect-back-end/handlers/post/authenticate-user/authenticateUser.ts
@@ -2,10 +2,20 @@ import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import pino from "pino";
 
+type AuthenticateUserRequest = {
+  email: string;
+  password: string;
+};
+
 export default function authenticateUser(databaseClient: PrismaClient) {
   const logger = pino({
     name: "handlers/post/authenticate-user/authenticateUser.ts",
   });
 
-  return async function (req: Request, res: Response) {};
+  return async function (req: Request, res: Response) {
+    const data: AuthenticateUserRequest = req.body;
+
+    try {
+    } catch (error) {}
+  };
 }
