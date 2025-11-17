d1318dc65a51b22a45c808377c971a834e5764d5
whiffy <bryanzhao168@yahoo.ca>
2025-11-16 19:25:15 -0500

    Added a context provider to keep track of the user id

---
+++ front_end/connect-front-end/src/app/_shared/user-id/UserIdContext.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useAuth } from "@clerk/nextjs";
+import React, {
+  createContext,
+  FC,
+  ReactNode,
+  useEffect,
+  useState,
+} from "react";
+import { authenticateUser } from "./_utilities/authenticateUser";
+import styles from "./UserIdContext.module.css";
+
+interface UserIdContextType {
+  userId: string;
+}
+
+interface UserIdContextProps {
+  children: ReactNode;
+}
+
+export const UserIdContext = createContext<UserIdContextType>({
+  userId: "",
+});
+
+export const UserIdContextProvider: FC<UserIdContextProps> = ({ children }) => {
+  const { getToken, isLoaded } = useAuth();
+  const [userId, setUserId] = useState<string>("");
+  const [error, setError] = useState<Error>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const userIdContextValue: UserIdContextType = {
+    userId: userId,
+  };
+
+  useEffect(() => {
+    if (!isLoaded) {
+      return;
+    }
+
+    const userId = localStorage.getItem("userId");
+    if (userId) {
+      setUserId(userId);
+      setLoading(false);
+      return;
+    }
+
+    authenticateUser(getToken)
+      .then((userId) => {
+        setUserId(userId);
+        localStorage.setItem("userId", userId);
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, [getToken, isLoaded]);
+
+  if (error) {
+    return (
+      <div
+        className={styles.PageNonContentContainer}
+      >{`Error occured while signing in: ${error.message}`}</div>
+    );
+  }
+
+  if (loading) {
+    return <div className={styles.PageNonContentContainer}>Loading...</div>;
+  }
+
+  return (
+    <UserIdContext.Provider value={userIdContextValue}>
+      {children}
+    </UserIdContext.Provider>
+  );
+};
--- front_end/connect-front-end/src/app/layout.tsx
+++ front_end/connect-front-end/src/app/layout.tsx
@@ -1,17 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
+import { UserIdContextProvider } from "./_shared/user-id/UserIdContext";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,10 +16,8 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
+        <body>
+          <UserIdContextProvider>{children}</UserIdContextProvider>
         </body>
       </html>
     </ClerkProvider>
