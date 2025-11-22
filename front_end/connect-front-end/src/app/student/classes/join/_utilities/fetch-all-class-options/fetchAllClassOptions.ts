f1bea34a42f27859a7798e13a0265c8362192da9
whiffy <bryanzhao168@yahoo.ca>
2025-11-21 22:45:09 -0500

    Added the utility to fetch and format all classes in the proper format for the drop-down

--- front_end/connect-front-end/src/app/student/classes/join/_utilities/fetch-all-class-options/fetchAllClassOptions.ts
+++ front_end/connect-front-end/src/app/student/classes/join/_utilities/fetch-all-class-options/fetchAllClassOptions.ts
@@ -1,10 +1,23 @@
 import getServerUrl from "../../../../../../../utilities/fetchApiUrl";
 
+type Teacher = {
+  teacherId: string;
+  firstName: string;
+  lastName: string;
+  createdAt: Date;
+};
+
 export type Class = {
   classId: string;
   courseName: string;
   studentGradeYear: number;
   createdAt: string;
+  teachers: Teacher[];
+};
+
+export type Option = {
+  value: string;
+  label: string;
 };
 
 type FetchAllClassesResponse = {
@@ -13,7 +26,7 @@ type FetchAllClassesResponse = {
   data: Class[];
 };
 
-export async function fetchAllClasses() {
+export async function fetchAllClassOptions() {
   const serverUrl = getServerUrl();
   const allClasses = await fetch(serverUrl + "/classes");
 
@@ -29,5 +42,18 @@ export async function fetchAllClasses() {
     throw new Error(`Failed to fetch all classes`);
   }
 
-  return response.data;
+  return response.data.map((avaliableClass) => {
+    let label = `${avaliableClass.courseName} - `;
+    if (avaliableClass.teachers.length > 0) {
+      const teacher = avaliableClass.teachers[0];
+      label += teacher.firstName + " " + teacher.lastName;
+    }
+
+    const option: Option = {
+      value: avaliableClass.classId,
+      label: label,
+    };
+
+    return option;
+  });
 }
