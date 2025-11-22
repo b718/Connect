b5f7b862e7a477cac167e6d756aff831376e57b3
whiffy <bryanzhao168@yahoo.ca>
2025-11-21 22:28:05 -0500

    Added an utility to register student in new class from the front-end

---
+++ front_end/connect-front-end/src/app/student/classes/join/_utilities/create-student-registration-for-class/createStudentRegistrationForClass.ts
@@ -0,0 +1,41 @@
+import { StatusCodes } from "http-status-codes";
+import getServerUrl from "../../../../../../../utilities/fetchApiUrl";
+
+type CreateStudentRegistrationForClassResponse = {
+  statusCode: number;
+  message: string;
+};
+
+export async function createStudentRegistrationForClass(
+  classId: string,
+  studentId: string
+) {
+  const serverUrl = getServerUrl();
+  const createStudentRegistrationForClass = await fetch(
+    serverUrl + `/classes/${classId}/students/${studentId}/join/class`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  if (
+    !createStudentRegistrationForClass.ok &&
+    createStudentRegistrationForClass.status != StatusCodes.BAD_REQUEST
+  ) {
+    throw new Error(
+      `unable to register student for class: ${createStudentRegistrationForClass.statusText}`
+    );
+  }
+
+  let response: CreateStudentRegistrationForClassResponse;
+
+  try {
+    response = await createStudentRegistrationForClass.json();
+  } catch (error) {
+    throw new Error(`unable to register student for class`);
+  }
+
+  return response.statusCode;
+}
