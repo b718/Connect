fe556b628329a271de465be867ab9d3ccdf52eda
whiffy <bryanzhao168@yahoo.ca>
2025-11-27 21:00:10 -0500

    Fixed prisma init for the new version of prisma

--- back_end/connect-back-end/database/getDatabaseClient.ts
+++ back_end/connect-back-end/database/getDatabaseClient.ts
@@ -1,6 +1,10 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 export default function getDatabaseClient() {
-  const databaseClient = new PrismaClient();
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  });
+  const databaseClient = new PrismaClient({ adapter: adapter });
   return databaseClient;
 }
