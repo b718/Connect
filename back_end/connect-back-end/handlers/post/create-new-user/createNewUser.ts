0e602afb52578892b49939abca28a0e7666e5b2f
whiffy <bryanzhao168@yahoo.ca>
2025-12-06 23:47:58 -0500

    Updated create new user handler

--- back_end/connect-back-end/handlers/post/create-new-user/createNewUser.ts
+++ back_end/connect-back-end/handlers/post/create-new-user/createNewUser.ts
@@ -4,11 +4,102 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import pino from "pino";
 
+const teacherEmailAddresses = [""];
+
 type AuthenticateUserResponse = {
   statusCode: number;
   message: string;
   userId: string | null;
 };
+
+export default function createNewUser(databaseClient: PrismaClient) {
+  const successMessage = "successfully created user";
+  const errorMessage = "unsuccessfully created user";
+  const logger = pino({ name: __filename });
+
+  return async function (req: Request, res: Response) {
+    try {
+      const { userId } = getAuth(req);
+      if (!userId) {
+        return createResponse(
+          StatusCodes.UNAUTHORIZED,
+          errorMessage,
+          null,
+          res
+        );
+      }
+
+      const user = await clerkClient.users.getUser(userId);
+      const userRole = await createNewUserQuery(databaseClient, user);
+      const updateUser = await clerkClient.users.updateUserMetadata(userId, {
+        publicMetadata: {
+          role: userRole.userRole,
+        },
+      });
+
+      logger.info(
+        { firstName: user.firstName, lastName: user.lastName, role: userRole },
+        successMessage
+      );
+
+      createResponse(StatusCodes.OK, successMessage, userRole.id, res);
+    } catch (error) {
+      logger.info({ err: error }, errorMessage);
+
+      createResponse(
+        StatusCodes.INTERNAL_SERVER_ERROR,
+        errorMessage,
+        null,
+        res
+      );
+    }
+  };
+}
+
+function createResponse(
+  statusCode: number,
+  message: string,
+  userId: string | null,
+  res: Response
+) {
+  const response: AuthenticateUserResponse = {
+    statusCode,
+    message,
+    userId,
+  };
+
+  res.status(statusCode).json(response);
+}
+
+async function createNewUserQuery(databaseClient: PrismaClient, user: User) {
+  const userRole = isUserTeacher(user.emailAddresses[0].emailAddress)
+    ? Role.TEACHER
+    : Role.STUDENT;
+  const userData = {
+    clerkUserId: user.id,
+    email: user.emailAddresses[0].emailAddress,
+    firstName: user.firstName ?? "",
+    lastName: user.lastName ?? "",
+    role: userRole,
+  };
+  const newUser = await databaseClient.users.upsert({
+    where: {
+      clerkUserId: user.id,
+    },
+    update: userData,
+    create: userData,
+  });
+
+  let newCreatedUser;
+
+  if (userRole == Role.STUDENT) {
+    newCreatedUser = await createNewStudent(databaseClient, user, userRole);
+  } else {
+    newCreatedUser = await createNewTeacher(databaseClient, user, userRole);
+  }
+
+  return newCreatedUser;
+}
 
 function isUserTeacher(emailAddress: string) {
   return teacherEmailAddresses.includes(emailAddress);
@@ -56,98 +147,4 @@ async function createNewTeacher(
   });
 
   return { userRole: userRole, id: newTeacher.teacherId };
-}
-
-async function createNewUserQuery(databaseClient: PrismaClient, user: User) {
-  const userRole = isUserTeacher(user.emailAddresses[0].emailAddress)
-    ? Role.TEACHER
-    : Role.STUDENT;
-  const userData = {
-    clerkUserId: user.id,
-    email: user.emailAddresses[0].emailAddress,
-    firstName: user.firstName ?? "",
-    lastName: user.lastName ?? "",
-    role: userRole,
-  };
-  const newUser = await databaseClient.users.upsert({
-    where: {
-      clerkUserId: user.id,
-    },
-    update: userData,
-    create: userData,
-  });
-
-  let newCreatedUser;
-
-  if (userRole == Role.STUDENT) {
-    newCreatedUser = await createNewStudent(databaseClient, user, userRole);
-  } else {
-    newCreatedUser = await createNewTeacher(databaseClient, user, userRole);
-  }
-
-  return newCreatedUser;
-}
-
-function createAuthenticateUserResponse(
-  statusCode: number,
-  message: string,
-  userId: string | null,
-  res: Response
-) {
-  const response: AuthenticateUserResponse = {
-    statusCode,
-    message,
-    userId,
-  };
-
-  res.status(statusCode).json(response);
-}
-
-export default function createNewUser(databaseClient: PrismaClient) {
-  const successMessage = "successfully created user";
-  const errorMessage = "unsuccessfully created user";
-  const logger = pino({ name: __filename });
-
-  return async function (req: Request, res: Response) {
-    try {
-      const { userId } = getAuth(req);
-      if (!userId) {
-        return createAuthenticateUserResponse(
-          StatusCodes.UNAUTHORIZED,
-          errorMessage,
-          null,
-          res
-        );
-      }
-
-      const user = await clerkClient.users.getUser(userId);
-      const userRole = await createNewUserQuery(databaseClient, user);
-      const updateUser = await clerkClient.users.updateUserMetadata(userId, {
-        publicMetadata: {
-          role: userRole.userRole,
-        },
-      });
-
-      logger.info(
-        { firstName: user.firstName, lastName: user.lastName, role: userRole },
-        successMessage
-      );
-
-      createAuthenticateUserResponse(
-        StatusCodes.OK,
-        successMessage,
-        userRole.id,
-        res
-      );
-    } catch (error) {
-      logger.info({ err: error }, errorMessage);
-
-      createAuthenticateUserResponse(
-        StatusCodes.INTERNAL_SERVER_ERROR,
-        errorMessage,
-        null,
-        res
-      );
-    }
-  };
 }
