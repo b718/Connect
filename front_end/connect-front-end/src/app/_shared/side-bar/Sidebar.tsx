574d32245601f8a2ef72dd243cda2585888c66a4
whiffy <bryanzhao168@yahoo.ca>
2026-02-22 14:34:38 -0500

    Added mobile side-bar support

--- front_end/connect-front-end/src/app/_shared/side-bar/Sidebar.tsx
+++ front_end/connect-front-end/src/app/_shared/side-bar/Sidebar.tsx
@@ -1,69 +1,105 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import SidebarRow from "./SidebarRow";
 import styles from "./Sidebar.module.css";
 import { SignedIn, UserButton, useUser } from "@clerk/nextjs";
+import useWindowDimensions from "./useWindowDimensions";
 
 const Sidebar = () => {
+  const MOBILE_SIDEBAR_BREAK_POINT = 600;
+  const [sideBarOpen, setSideBarOpen] = useState<boolean>(false);
+  const { width } = useWindowDimensions();
   const { user } = useUser();
   const userRole = user?.publicMetadata.role as string;
+  const SidebarRows = () => (
+    <>
+      <div className={styles.SidebarSectionSeperatorText}>Home</div>
+      <SidebarRow
+        rowName={userRole ? userRole.toLowerCase() : ""}
+        rowIcon={
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        }
+      />
+      <SidebarRow
+        rowName={"Overview"}
+        rowIcon={
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9 21V13.6C9 13.0399 9 12.7599 9.109 12.546C9.20487 12.3578 9.35785 12.2049 9.54601 12.109C9.75993 12 10.04 12 10.6 12H13.4C13.9601 12 14.2401 12 14.454 12.109C14.6422 12.2049 14.7951 12.3578 14.891 12.546C15 12.7599 15 13.0399 15 13.6V21M2 9.5L11.04 2.72C11.3843 2.46181 11.5564 2.33271 11.7454 2.28294C11.9123 2.23902 12.0877 2.23902 12.2546 2.28295C12.4436 2.33271 12.6157 2.46181 12.96 2.72L22 9.5M4 8V17.8C4 18.9201 4 19.4802 4.21799 19.908C4.40974 20.2843 4.7157 20.5903 5.09202 20.782C5.51985 21 6.0799 21 7.2 21H16.8C17.9201 21 18.4802 21 18.908 20.782C19.2843 20.5903 19.5903 20.2843 19.782 19.908C20 19.4802 20 18.9201 20 17.8V8L13.92 3.44C13.2315 2.92361 12.8872 2.66542 12.5091 2.56589C12.1754 2.47804 11.8246 2.47804 11.4909 2.56589C11.1128 2.66542 10.7685 2.92361 10.08 3.44L4 8Z"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        }
+        redirectUrl={"/"}
+      />
+      <SidebarRow
+        rowName={"Classes"}
+        rowIcon={
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M11 11H6.2C5.07989 11 4.51984 11 4.09202 11.218C3.71569 11.4097 3.40973 11.7157 3.21799 12.092C3 12.5198 3 13.0799 3 14.2V21M21 21V6.2C21 5.0799 21 4.51984 20.782 4.09202C20.5903 3.71569 20.2843 3.40973 19.908 3.21799C19.4802 3 18.9201 3 17.8 3H14.2C13.0799 3 12.5198 3 12.092 3.21799C11.7157 3.40973 11.4097 3.71569 11.218 4.09202C11 4.51984 11 5.0799 11 6.2V21M22 21H2M14.5 7H17.5M14.5 11H17.5M14.5 15H17.5"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        }
+        redirectUrl={"classes"}
+      />
+    </>
+  );
+  const DesktopSidebar = () => (
+    <div className={styles.SidebarInnerContainer}>
+      <SidebarRows />
+    </div>
+  );
+  const MobileSidebar = () => (
+    <>
+      <div
+        className={styles.SidebarBurgerMenu}
+        onClick={() => setSideBarOpen(!sideBarOpen)}
+      >
+        <svg width="16" height="10" viewBox="0 0 16 10">
+          <rect y="8" width="16" height="2" rx="1"></rect>
+          <rect y="4" width="16" height="2" rx="1"></rect>
+          <rect width="16" height="2" rx="1"></rect>
+        </svg>
+      </div>
+
+      {sideBarOpen && (
+        <div className={styles.SidebarInnerContainer}>
+          <SidebarRows />
+        </div>
+      )}
+    </>
+  );
 
   return (
     <div className={styles.SidebarContainer}>
-      <div className={styles.SidebarInnerContainer}>
-        <div className={styles.SidebarSectionSeperatorText}>Home</div>
-        <SidebarRow
-          rowName={userRole ? userRole.toLowerCase() : ""}
-          rowIcon={
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          }
-        />
-        <SidebarRow
-          rowName={"Overview"}
-          rowIcon={
-            <svg
-              width="100%"
-              height="100%"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9 21V13.6C9 13.0399 9 12.7599 9.109 12.546C9.20487 12.3578 9.35785 12.2049 9.54601 12.109C9.75993 12 10.04 12 10.6 12H13.4C13.9601 12 14.2401 12 14.454 12.109C14.6422 12.2049 14.7951 12.3578 14.891 12.546C15 12.7599 15 13.0399 15 13.6V21M2 9.5L11.04 2.72C11.3843 2.46181 11.5564 2.33271 11.7454 2.28294C11.9123 2.23902 12.0877 2.23902 12.2546 2.28295C12.4436 2.33271 12.6157 2.46181 12.96 2.72L22 9.5M4 8V17.8C4 18.9201 4 19.4802 4.21799 19.908C4.40974 20.2843 4.7157 20.5903 5.09202 20.782C5.51985 21 6.0799 21 7.2 21H16.8C17.9201 21 18.4802 21 18.908 20.782C19.2843 20.5903 19.5903 20.2843 19.782 19.908C20 19.4802 20 18.9201 20 17.8V8L13.92 3.44C13.2315 2.92361 12.8872 2.66542 12.5091 2.56589C12.1754 2.47804 11.8246 2.47804 11.4909 2.56589C11.1128 2.66542 10.7685 2.92361 10.08 3.44L4 8Z"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          }
-          redirectUrl={"/"}
-        />
-        <SidebarRow
-          rowName={"Classes"}
-          rowIcon={
-            <svg
-              width="100%"
-              height="100%"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M11 11H6.2C5.07989 11 4.51984 11 4.09202 11.218C3.71569 11.4097 3.40973 11.7157 3.21799 12.092C3 12.5198 3 13.0799 3 14.2V21M21 21V6.2C21 5.0799 21 4.51984 20.782 4.09202C20.5903 3.71569 20.2843 3.40973 19.908 3.21799C19.4802 3 18.9201 3 17.8 3H14.2C13.0799 3 12.5198 3 12.092 3.21799C11.7157 3.40973 11.4097 3.71569 11.218 4.09202C11 4.51984 11 5.0799 11 6.2V21M22 21H2M14.5 7H17.5M14.5 11H17.5M14.5 15H17.5"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          }
-          redirectUrl={"classes"}
-        />
-      </div>
+      {width > MOBILE_SIDEBAR_BREAK_POINT ? (
+        <DesktopSidebar />
+      ) : (
+        <MobileSidebar />
+      )}
     </div>
   );
 };
