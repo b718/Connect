dcc11f9bb4ddc5f86fd953e301b36d67b06917ec
whiffy <bryanzhao168@yahoo.ca>
2026-03-07 23:19:14 -0800

    Chnaged api fetching logic

--- front_end/connect-front-end/utilities/fetchApiUrl.ts
+++ front_end/connect-front-end/utilities/fetchApiUrl.ts
@@ -1,5 +1,5 @@
 export default function getServerUrl() {
-  if (process.env.NEXT_PUBLIC_DEV == "true" || !process.env.NEXT_PUBLIC_DEV) {
+  if (!process.env.NEXT_PUBLIC_SERVER_URL_PROD) {
     return "http://localhost:3003";
   }
 
