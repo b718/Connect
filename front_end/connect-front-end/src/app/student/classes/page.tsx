744cfd1a92c583746e3a48bfd3a2f4411becc337
whiffy <bryanzhao168@yahoo.ca>
2026-03-07 21:29:48 -0800

    [POC] - Resizable panels

--- front_end/connect-front-end/src/app/student/classes/page.tsx
+++ front_end/connect-front-end/src/app/student/classes/page.tsx
@@ -3,13 +3,25 @@ import styles from "./page.module.css";
 import Sidebar from "@/app/_shared/side-bar/Sidebar";
 import MainScreen from "@/app/_shared/main-screen/MainScreen";
 import DisplayStudentClasses from "./_components/DisplayStudentClasses";
+import { Group, Panel } from "react-resizable-panels";
 
 const page = () => {
+  // return (
+  //   <div className={styles.PageContainer}>
+  //     <Sidebar />
+
+  //     <MainScreen component={<DisplayStudentClasses />} />
+  //   </div>
+
   return (
-    <div className={styles.PageContainer}>
-      <Sidebar />
-      <MainScreen component={<DisplayStudentClasses />} />
-    </div>
+    <Group>
+      <Panel defaultSize={"20%"} minSize={"20%"}>
+        <Sidebar />
+      </Panel>
+      <Panel>
+        <MainScreen component={<DisplayStudentClasses />} />
+      </Panel>
+    </Group>
   );
 };
 
