4f9046298d1122ac884f7107d71438f6372b268a
whiffy <bryanzhao168@yahoo.ca>
2026-03-09 21:01:20 -0700

    Updated database seeding data

--- back_end/connect-back-end/seed/seedDatabase.ts
+++ back_end/connect-back-end/seed/seedDatabase.ts
@@ -81,6 +81,8 @@ async function seedDatabase(databaseClient: PrismaClient) {
       },
     });
 
+    console.log(sampleTeacher);
+
     const sampleStudentOne = await databaseClient.students.create({
       data: {
         firstName: sampleStudents[0].firstName,
@@ -144,11 +146,14 @@ async function seedDatabase(databaseClient: PrismaClient) {
             testsTableId: sampleTestOne.testId,
             studentsTableId: sampleStudentOne.studentId,
             testGrade: 95,
+            gradeResultReasoning: "Perfect.",
           },
           {
             testsTableId: sampleTestTwo.testId,
             studentsTableId: sampleStudentOne.studentId,
-            testGrade: 69,
+            testGrade: 0,
+            gradeResultReasoning:
+              "The provided answer key (Page 558 - Logarithmic and Exponential equations) and the student submission (Page 34 - Systems of Three Linear Equations) are for completely different assignments. As the 'Source of Truth' key does not contain any of the problems or answers found in the student's work, no points can be awarded based on a direct comparison.",
           },
         ],
       });
@@ -160,11 +165,14 @@ async function seedDatabase(databaseClient: PrismaClient) {
             testsTableId: sampleTestOne.testId,
             studentsTableId: sampleStudentTwo.studentId,
             testGrade: 93,
+            gradeResultReasoning: "Perfect.",
           },
           {
             testsTableId: sampleTestTwo.testId,
             studentsTableId: sampleStudentTwo.studentId,
-            testGrade: 95,
+            testGrade: 55,
+            gradeResultReasoning:
+              "Suspendisse tristique velit vitae nibh sollicitudin bibendum. Sed at dictum lectus, id suscipit tortor. Aenean dapibus, erat sed commodo aliquet, felis eros vehicula quam, at sodales ante nibh in ligula. Nam dolor mauris, placerat quis sem sed, tempor commodo urna. Vestibulum sed dapibus diam, a imperdiet est. Cras semper eleifend libero sed lobortis. In tempus volutpat tellus, eget efficitur ex aliquam et. Ut laoreet ipsum quis augue bibendum vulputate. In a accumsan justo. Donec sagittis ligula eu nisi euismod ornare. Praesent dignissim nisi magna. In dolor dolor, molestie eu libero suscipit, faucibus condimentum sem. In in porta tortor. Pellentesque vitae ultricies sem. Quisque pharetra tristique arcu pulvinar malesuada. Morbi eget est fringilla, varius enim eget, faucibus quam.",
           },
         ],
       });
